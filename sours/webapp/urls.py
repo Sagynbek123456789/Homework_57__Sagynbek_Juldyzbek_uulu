c5d74e4cc32651163910fe98d00de0631297d5f4
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-27 23:51:31 +0600

    Добавил новые URLS

--- sours/webapp/urls.py
+++ sours/webapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from webapp.views.issues import IndexView, IssueView, IssueCreateView, IssueUpdateView, IssueDeleteView
+from webapp.views import IssueView, IssueCreateView, IssueUpdateView, IssueDeleteView, ProjectListView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', ProjectListView.as_view(), name='index'),
     path('issues/<int:pk>', IssueView.as_view(), name='issue_view'),
     path('issues/add/', IssueCreateView.as_view(), name='issue_add_view'),
     path('issues/<int:pk>/edit/', IssueUpdateView.as_view(), name='issue_update_view'),
