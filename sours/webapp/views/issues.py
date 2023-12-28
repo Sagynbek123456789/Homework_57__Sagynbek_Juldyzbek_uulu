0777625c0857ee77364d0a135d480c9c4b1bdd67
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-28 21:17:50 +0600

    Внес  дополнитеьные изменения в код DeleteView, UpdateView

--- sours/webapp/views/issues.py
+++ sours/webapp/views/issues.py
@@ -1,19 +1,20 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import TemplateView, View, CreateView
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.views.generic import TemplateView, View, CreateView, UpdateView, DetailView, DeleteView
 from webapp.models import Issue, Project
 from webapp.forms import IssueForm
 
 
 # Create your views here.
 
-class IssueView(TemplateView):
+class IssueView(DeleteView):
     template_name = 'issues/issue_view.html'
+    model = Issue
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
-        context['issue'] = issue
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
+    #     context['issue'] = issue
+    #     return context
 
 
 class IssueCreateView(CreateView):
@@ -42,35 +43,46 @@ class IssueCreateView(CreateView):
     #     return render(request, 'issues/issue_create.html', {'form': form})
 
 
-class IssueUpdateView(View):
-    def dispatch(self, request, *args, **kwargs):
-        self.issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
-        return super().dispatch(request, *args, **kwargs)
+class IssueUpdateView(UpdateView):
+    template_name = 'issues/issue_update.html'
+    model = Issue
+    form_class = IssueForm
+# class IssueUpdateView(View):
+#     def dispatch(self, request, *args, **kwargs):
+#         self.issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     def get(self, request, *args, **kwargs):
+#         form = IssueForm(instance=self.issue)
+#         return render(request, 'issues/issue_update.html', {'form': form})
+#
+#     def post(self, request, *args, **kwargs):
+#         form = IssueForm(instance=self.issue, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('issue_view', pk=self.issue.pk)
+#         return render(request, 'issues/issue_update.html', {'form': form})
 
-    def get(self, request, *args, **kwargs):
-        form = IssueForm(instance=self.issue)
-        return render(request, 'issues/issue_update.html', {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = IssueForm(instance=self.issue, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('issue_view', pk=self.issue.pk)
-        return render(request, 'issues/issue_update.html', {'form': form})
+    def get_success_url(self):
+        return reverse('project_view', kwargs={'pk': self.object.project.pk})
 
 
-class IssueDeleteView(TemplateView):
+class IssueDeleteView(DeleteView):
     template_name = 'issues/issue_delete.html'
+    model = Issue
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
-        context['issue'] = issue
-        return context
+    def get_success_url(self):
+        return reverse('project_view', kwargs={'pk': self.object.project.pk})
 
-    def post(self, request, *args, **kwargs):
-        issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
-        issue.delete()
-        return redirect('index')
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
+    #     context['issue'] = issue
+    #     return context
+    #
+    # def post(self, request, *args, **kwargs):
+    #     issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
+    #     issue.delete()
+    #     return redirect('index')
 
 
