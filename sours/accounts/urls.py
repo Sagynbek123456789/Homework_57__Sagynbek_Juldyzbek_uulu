80fe7d77e78214f14210df8e5ff933daecacc086
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-30 18:16:48 +0600

    Создали представление LOGIN и добавили представление в него а также добавили в URLS

--- sours/accounts/urls.py
+++ sours/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from accounts.views import login_view
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/')
+    path('login/', login_view, name='login')
 
 ]
